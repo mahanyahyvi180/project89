e417abf999e5ccc3a279055a83a63952f5a6b0ec
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 15:41:29 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp89
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account("Jane Green");
+            Account1 account2 = new Account("John Blue");
+
+            Console.WriteLine($"account1 name is:{account1.Name}");
+            Console.WriteLine($"account2 name is:{account2.Name}");
+        }
+    }
+}
